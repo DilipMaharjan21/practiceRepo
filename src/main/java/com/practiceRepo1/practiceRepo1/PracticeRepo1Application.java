67f5743a1c92c791044e52c69a10cbe60412199c
Dilip Maharjan <dilipmaharjan76@gmail.com>
2021-05-19 20:30:53 -0500

    added src and pom file

---
+++ src/main/java/com/practiceRepo1/practiceRepo1/PracticeRepo1Application.java
@@ -0,0 +1,13 @@
+package com.practiceRepo1.practiceRepo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeRepo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeRepo1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/practiceRepo1/practiceRepo1/PracticeRepo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practiceRepo1.practiceRepo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeRepo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
